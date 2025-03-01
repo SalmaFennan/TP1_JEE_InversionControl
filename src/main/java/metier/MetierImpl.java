50762a6e5fc935eec9b325bd5fd8285108f2f4c7
SalmaFennan <salmafennan19@gmail.com>
2025-03-01 21:03:34 +0100

    Injection de dépendance par instanciation statique

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,6 +1,6 @@
 package metier;
 
-import dao.DaoImpl;
+
 import dao.IDao;
 
 public class MetierImpl implements IMetier {
@@ -8,8 +8,7 @@ public class MetierImpl implements IMetier {
     @Override
     public double calcul() {
         double t= dao.getData();
-        double res=t*23;
-        return res;
+        return t*23;
     }
     public void setDao(IDao dao) {
         this.dao = dao;
