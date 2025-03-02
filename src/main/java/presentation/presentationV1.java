904b7851df98d35b55f2213ef55387cb1e50e45a
SalmaFennan <salmafennan19@gmail.com>
2025-03-02 14:03:53 +0100

    Injection de dépendance par instanciation dynamique

--- src/main/java/presentation/presentationV1.java
+++ src/main/java/presentation/presentationV1.java
@@ -7,8 +7,8 @@ import metier.MetierImpl;
 public class presentationV1 {
     public static void main(String[] args) {
         DaoImpl dao = new DaoImpl();
-        MetierImpl m = new MetierImpl();
-        m.setDao(dao);
+        MetierImpl m = new MetierImpl(dao);//injection des dependances via le constructeur
+        m.setDao(dao);//injection via le setter
         System.out.println("Res="+m.calcul());
     }
 }
